17ad9e66917cbedfdb7fc53790d0289a3e1eb4c0
si39 <sijuta01@gmail.com>
2021-02-19 01:21:51 +0000

    "(18-02) Arrow functions e objetos"

--- revisao-js/arrow-function.js
+++ revisao-js/arrow-function.js
@@ -18,3 +18,73 @@ console.log(quadrado(9))
 const quadrado2 = n => n * n
 console.log(quadrado(9))
 
+//FUNÇÃO MAIS DE UM PARÂMETRO
+function potencia(b, e) { //b = base, e=expoente
+    return b**e
+
+}
+console.log(potencia(2, 8))
+
+//FUNÇÃO EQUIVALENTE NO FORMATO arrow function
+
+//QUANDO TEMOS MAIS DE UM PARÂMETRO, OS PARÊNTESES EM VOLTA
+// DOS ARGUMENTOS VOLTAM A SER OBRIGATÓRIOS
+
+letpotencia2 = (b, e) => b ** e
+console.log(potencia(2, 8))
+
+// FUNÇÃO SEM PARÂMENTRO
+function megasena() {
+    //ESTA FUNÇÃO RETORNA UM NÚMERO ALEATÓRIO ENTRE 1 2 60
+    //Math.random() -> retorna um número aleatório entre 0 (inclusive) e 1 (exclusive)
+    //*60 -> ajusta a faixa de valores para entre 0 e 59
+    // o + 1 -> ajusta a faixa de valores para entre 1 e 60
+    //o Math.floor -> arredonda para o menor inteiro ( tira as casas decimais)
+
+    return Math.floor(Math.randow() * 60 + 1
+} 
+console.log(megasena(), megasena(), megasena(),megasena(),megasena(),megasena())
+
+//QUANDO A FUNÇÃO NÃO TEM ARGUMENTOS , NA arrow function é neessário deixa
+//um par de parênteses vazios para marcar seu lugar
+const megasena = () => Math.floor(Math.random() * 60 + 1)
+console.log(megasena2(), megasena2(), megasena2(),megasena2(),megasena2(),megasena2())
+
+//FUNÇÃO COM MAIS DE UMA LINHA DE CÓDIGO NO CORPO
+function somaVet(vet){
+    let soma = 0
+    for(let n of vet) soma += n
+    return soma
+}
+console.log(somaVet([12, 42, -11, 20, 9, 16]))
+
+//QUANDO A FUNÇÃO TEM MAIS DE UMA LINHA DE CÓDIGO, NA arrow function, EQUIVALENTE
+//RETORNAM AS CHAVES E TAMBÉM A PALAVRA return, caso ela retorne valor
+
+const somaVet2 = vet =>{
+    let soma = 0
+    for (let n of vet) soma += n
+    return soma
+}
+console.log(somaVet([12, 42, -11, 20, 9, 16]))
+
+//CONCLUSÃO: Arrow function foram concebidas para facilitar a escrita de
+//funções de apenas uma linha, embora também possam ser usadas para funções
+// de múltiplas linhas.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ revisao-js/objetos.js
@@ -0,0 +1,97 @@
+//OBJETOS SÃO ESTRUTURAS QUE PERMITEM ARMAZENAR DIVERSOS VALORES 
+// EM UMA MESMA VARIÁVEL, PERMITINDO ACESSÁ-LOS POR MEIO DE UM NOME
+
+// CRIAÇÃO DE UM OBJETO VAZIO
+let vazio1 = {} //forma moderna do JAVASCRIPT
+
+let vazio2 = new Object() //forma antiga/ tradicional  do JAVASCRIPT
+
+//criando um objeto com dados
+let pessoa = {
+    //OS DADOS SÃO ARMAZENADOS NO FORMATO
+    //PROPRIEDADE: VALOR
+    //ATRIBUTO: VALOR
+    nome: 'Orkiyilson de Oliveira',
+    sexo: 'M',
+    dataNasc: '2008-04-19',
+    ocupacao: 'estudante',
+    //depois da propriedade, nomes, pode ter espaços ou acentos,
+    //nesse caso o nome da propriedade precisa estar entre aspas
+    'cidade de origem': 'Franca/SP',
+    gostos: ['jogo', 'doces', 'dormir'] // VETOR EM UMA POSIÇÃO DE OBJETO
+}
+console.log(pessoa)
+console.table(pessoa)
+
+//EXIBINDO APENAS UMA PROPRIEDADE, POR EXEMPLO EXIBIR APENAS O NOME
+//PROPRIEDADE SÃO AS "PARTES" DO FORMULÁRIO POR EXEMPLO
+//NO OBJETO AS PROPRIEDADES SÃO NOMEADAS, OU SEJA TEM NOMES
+console.log(pessoa.nome)
+console.log(pessoa.ocupacao)
+
+console.log(pessoa.cidade de origem) //QUANDO A PROPRIEDADE TEM ESPAÇOS OU ACENTOS, NÃO É POSSÍVEL
+//USAR A SINTAXE DE PONTO. É NECESSÁRIO USAR A SINTAXE DE COLCHETES.
+
+console.log(pessoa['cidade de origem'])
+
+console.log(pessoa.gostos)
+
+//A SINTAXE DOS COLCHETES SEMPRE FUNCIONA MESMO SE O NOME DA PROPRIEDADE FOR UMA ÚNICA PALAVRA SEM ACENTO
+console.log(['nome'])
+
+console.log(pessoa['gostos'][1])//usa o [] vetor, e mostra apenas o segundo gosto
+
+//A SINTAXE DOS COLCHETES TEM OUTRA CONSEQUÊNCIA, É A POSSIBILIDADE
+//DE USAR NOMES DE PROPRIEDADES QUE ESTÃO DENTRO DAS VARIÁVEIS
+
+let x ='sexo'
+console.log(pessoa[x])
+x = 'dataNasc'
+console.log(pessoa[x])
+
+//pode-se criar uma propriedade para um objeto já existente
+//basta atribuir um valor a uma nome de propriedade ainda não existente
+pessoa.email = 'orkiyilson.com'
+pessoa.celular = '(16) 9999-4567'
+console.table(pessoa)
+
+//criando um objeto vazio e só depois adicionando propriedades
+let carro = {}
+carro.marca = 'Volkswagen'
+carro.modelo = 'Kombi'
+carro.ano = 1971
+carro['combustível'] = 'gasolina'
+carro.cor = 'bege'
+
+console.table(carro)
+
+//excluindo propriedade de um objeto
+delete carro.ano
+
+console.table(carro)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
