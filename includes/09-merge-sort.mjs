dd00329db77c9f53174eebbab94ddd40f81515f2
si39 <sijuta01@gmail.com>
2021-03-22 17:19:31 +0000

    (18/03) Término do merge sort

--- includes/09-merge-sort.mjs
+++ includes/09-merge-sort.mjs
@@ -6,8 +6,64 @@
     dessa vez com os elementos já em ordem.
 */
 
-function mergeSort(vetor) {
+//function mergeSort(vetor) {
 
+   // function mesclar(vetEsq, vetDir) {
+       // let pEsq = 0, pDir = 0, vetRes = []
+        //while(pEsq < vetEsq.length && pDir < vetDir.length) {
+            //if(vetEsq[pEsq] < vetDir[pDir]) {
+               // vetRes.push(vetEsq[pEsq])
+                //pEsq++
+           // }
+           // else {
+           //    vetRes.push(vetDir[pDir])
+           //    pDir++
+           // }
+        //}
+        // Descobrir de qual lado sobrou
+        //let sobra
+
+        // Sobra à esquerda
+        //if(pEsq < pDir) sobra = vetEsq.slice(pEsq)
+        // Sobra à direita
+        //else sobra = vetDir.slice(pDir)
+
+        // Retorna vetor de resultados + sobra
+        //return [...vetRes, ...sobra]
+   // }
+
+    // Para ser "desmontado", um vetor deve ter, pelo menos, 2 elementos
+    //if(vetor.length > 1) {
+    //    const meio = Math.floor(vetor.length / 2)
+        // slice() fatia um vetor, copiando os elementos desde a posição inicial informada
+        // (inclusive) até a posição final (exclusive - a posição final NÃO entra)
+     //   let vetEsq = vetor.slice(0, meio)
+        // Caso o segundo parâmetro de slice() seja omitido, serão copiados os elementos
+        // desde a posição informada até a posição final
+       // let vetDir = vetor.slice(meio)
+
+       // console.log({vetEsq, vetDir})
+
+        // Chamadas recursivas à função
+        //vetEsq = mergeSort(vetEsq)
+        //vetDir = mergeSort(vetDir)
+
+        //const vetFinal = mesclar(vetEsq, vetDir)
+        //console.log({vetFinal})
+
+        //return vetFinal
+    //}
+    //return vetor    // Vetor de 1 elemento, não modificado
+//}
+
+//let nums = [7, 4, 9, 0, 6, 1, 8, 2, 5, 3]
+//let numsOrd = mergeSort(nums)
+//console.log({numsOrd})
+
+let comps, divisoes, juncoes
+
+function mergeSort(vetor) {
+    
     function mesclar(vetEsq, vetDir) {
         let pEsq = 0, pDir = 0, vetRes = []
         while(pEsq < vetEsq.length && pDir < vetDir.length) {
@@ -19,6 +75,7 @@ function mergeSort(vetor) {
                 vetRes.push(vetDir[pDir])
                 pDir++
             }
+            comps++
         }
         // Descobrir de qual lado sobrou
         let sobra
@@ -31,7 +88,7 @@ function mergeSort(vetor) {
         // Retorna vetor de resultados + sobra
         return [...vetRes, ...sobra]
     }
-
+    
     // Para ser "desmontado", um vetor deve ter, pelo menos, 2 elementos
     if(vetor.length > 1) {
         const meio = Math.floor(vetor.length / 2)
@@ -41,21 +98,38 @@ function mergeSort(vetor) {
         // Caso o segundo parâmetro de slice() seja omitido, serão copiados os elementos
         // desde a posição informada até a posição final
         let vetDir = vetor.slice(meio)
+        divisoes++
 
-        console.log({vetEsq, vetDir})
+        //console.log({vetEsq, vetDir})
 
         // Chamadas recursivas à função
         vetEsq = mergeSort(vetEsq)
         vetDir = mergeSort(vetDir)
 
         const vetFinal = mesclar(vetEsq, vetDir)
-        console.log({vetFinal})
-
+        juncoes++
+        //console.log({vetFinal})
+        
         return vetFinal
     }
-    return vetor    // Vetor de 1 elemento, não modificado
+    return vetor    // Vetor de 1 elemento, não modificado, condição de saída
 }
 
-let nums = [7, 4, 9, 0, 6, 1, 8, 2, 5, 3]
+comps = 0, divisoes = 0, juncoes = 0
+//let nums = [7, 4, 9, 0, 6, 1, 8, 2, 5, 3]
+//let nums = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+let nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 let numsOrd = mergeSort(nums)
 console.log({numsOrd})
+console.log({comps, divisoes, juncoes})
+
+
+import { nomes } from './includes/100-mil-nomes.mjs'
+
+comps = 0, divisoes = 0, juncoes = 0
+console.time('Ordenando nomes...')
+const nomesOrd = mergeSort(nomes)
+console.timeEnd('Ordenando nomes...')
+let memoria = process.memoryUsage().heapUsed / 1024 / 1024
+console.log('DEPOIS:', nomesOrd)
+console.log({comps, divisoes, juncoes, memoria})
