41af659a5293371a542842d61ff52852c518a070
si39 <sijuta01@gmail.com>
2021-04-06 13:05:50 +0000

    (05/04) Início da estrutura pilha

---
+++ revisao-js/16-pilha2.mjs
@@ -0,0 +1,20 @@
+import { Stack } from './lib/Stack.mjs'
+
+const texto = 'Socorram-me, subi no ônibus em Marrocos'
+
+const pilha = new Stack()
+
+// Empilhamento
+for(let i = 0; i < texto.length; i++) {
+    pilha.push(texto.charAt(i))
+}
+
+console.log(pilha.print())
+
+let textoRev = ''
+
+// Desempilhamento
+while(! pilha.empty) { 
+    textoRev += pilha.pop()
+}
+console.log(textoRev)
