1d79eadf87dcc061dabf8bb4dff183bff5e36cef
si39 <sijuta01@gmail.com>
2021-04-16 01:32:30 +0000

    (15/04) Prova Bimestral

--- revisao-js/algoritmo1.mjs
+++ revisao-js/algoritmo1.mjs
@@ -1,10 +1,34 @@
-function k(m, n) {
-    let o = 0, p = m.length - 1, q
-    while(p >= o) {
-        q = Math.floor((p + o) / 2)
-        if(m[q] === n) return q
-        else if(n > m[q]) o = q + 1
-        else p = q - 1
+function k(m, n, o) {    
+    if(o <= n) return // condição de saída das chamadas recursivas
+    const p = o    
+    let q = n - 1
+
+        for(let i = n; i < o; i++) {
+        if(n(m[p],  m[i])) { // estavam invertidos
+            q++
+            if(i !== q) {
+                [ m[i], m[q] ] = [ m[q], m[i] ]
+        }
     }
-    return -1  // Não existe
+   }
+    q++
+    if(n(m[q], m[p])) {
+        [ m[p], m[q] ] = [ m[q], m[p] ]
+        q++
+        k(m, n, q - 1)
+    k(m, q + 1, o)
 }
+}
+
+
+// Algoritmo : QUICK SORT
+// Colocar mais um parâmetro ( 4 no total para ordenar )
+// Esse parâmetro nós acrescentamos para comparar
+// ESCOLHE UM DOS ELEMENTOS DO VETOR PARA SER O PIVÔ
+// k : ordena
+// m : vetor a ser ordenado
+// n: início do vetor
+// 0 : fim do vetor
+// q : variável que corresponde a div
+// i : contador , inicia em 0 e vai até a penúltima posição.
+
